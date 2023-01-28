c95ba745946ca66b353f9212cef093a481eb7176
josu_shell <yoy07030@khu.ac.kr>
2023-01-28 15:45:58 +0900

    Initial Commit

---
+++ momoIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  momoIOS
+//
+//  Created by JOSUEYEON on 2023/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
