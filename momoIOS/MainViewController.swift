46280498efc3eb256c8d38b18669faf229b951e9
josu_shell <yoy07030@khu.ac.kr>
2023-01-28 16:42:59 +0900

    [FEAT] session main view controller

---
+++ momoIOS/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  momoIOS
+//
+//  Created by JOSUEYEON on 2023/01/28.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    
+}
+
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+}
