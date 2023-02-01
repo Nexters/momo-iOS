503813203efd74ab8b812708b0034d0bfe93857d
josu_shell <yoy07030@khu.ac.kr>
2023-02-01 13:31:34 +0900

    [FEAT][#1] 메인 세션 불참 cell

---
+++ MainSession/MainSessionInfoCell.swift
@@ -0,0 +1,8 @@
+//
+//  MainSessionInfoCell.swift
+//  momoIOS
+//
+//  Created by JOSUEYEON on 2023/02/01.
+//
+
+import Foundation
---
+++ MainSession/MainSessionNoInfoCell.swift
@@ -0,0 +1,8 @@
+//
+//  MainSessionNoInfoCell.swift
+//  momoIOS
+//
+//  Created by JOSUEYEON on 2023/02/01.
+//
+
+import Foundation
