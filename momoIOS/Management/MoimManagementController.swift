8e6fb419d9733c06b4904f97be279a6c55a87f1a
dayo2n <0217dayun@naver.com>
2023-02-09 22:04:25 +0900

    [CHORE][#34] 파일명과 컨트롤러명이 일치하도록 수정

---
+++ momoIOS/Management/MoimManagementController.swift
@@ -0,0 +1,22 @@
+//
+//  MoimManageController.swift
+//  momoIOS
+//
+//  Created by 문다 on 2023/02/09.
+//
+
+import UIKit
+import SnapKit
+
+class MoimManagementcontrolelr: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let profileView = ProfileView()
+    
+    // MARK: - Lifecycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
