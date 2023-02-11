cf8bdec3758569b63fb3726cf0d15e3f911cc28c
dayo2n <0217dayun@naver.com>
2023-02-11 11:43:07 +0900

    [STYLE][#34] 회원관리/모임관리 구분을 위해 폴더명 변경

--- momoIOS/ViewController.swift
+++ momoIOS/ViewController.swift
@@ -47,6 +47,6 @@ class ViewController: UIViewController {
     }
     
     @objc private func goToOtherViewController() {
-        self.navigationController?.pushViewController(MoimManagementcontrolelr(), animated: true)
+        self.navigationController?.pushViewController(RegistrationController(), animated: true)
     }
 }
