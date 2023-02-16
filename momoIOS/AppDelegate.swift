581ff5d44ba6032303e61b4945baa4f97463f8a6
dayo2n <0217dayun@naver.com>
2023-02-16 21:47:56 +0900

    [CHORE][#54] Google Maps API 연동

--- momoIOS/AppDelegate.swift
+++ momoIOS/AppDelegate.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import GooglePlaces
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSPlacesClient.provideAPIKey(Bundle.main.googleMapsAPIKey)
         return true
     }
 
---
+++ momoIOS/momo++Bundle.swift
@@ -0,0 +1,18 @@
+//
+//  momo++Bundle.swift
+//  momoIOS
+//
+//  Created by 문다 on 2023/02/16.
+//
+
+import Foundation
+
+extension Bundle {
+    var googleMapsAPIKey: String {
+        guard let file = self.path(forResource: "API", ofType: "plist") else { return "" }
+        
+        guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
+        guard let key = resource["GOOGLE_MAPS_API_KEY"] as? String else { fatalError("API.plist 내 API KEY 설정 필요")}
+        return key
+    }
+}
