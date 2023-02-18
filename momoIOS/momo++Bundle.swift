0c6c3a7cb1994fe5745566c88cb6e4acb00c246a
dayo2n <0217dayun@naver.com>
2023-02-18 16:31:39 +0900

    [FIX][#56] 공백을 추가해 Lint 에러 해결

--- momoIOS/momo++Bundle.swift
+++ momoIOS/momo++Bundle.swift
@@ -12,7 +12,7 @@ extension Bundle {
         guard let file = self.path(forResource: "API", ofType: "plist") else { return "" }
         
         guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
-        guard let key = resource["GOOGLE_MAPS_API_KEY"] as? String else { fatalError("API.plist 내 API KEY 설정 필요")}
+        guard let key = resource["GOOGLE_MAPS_API_KEY"] as? String else { fatalError("API.plist 내 API KEY 설정 필요") }
         return key
     }
 }
