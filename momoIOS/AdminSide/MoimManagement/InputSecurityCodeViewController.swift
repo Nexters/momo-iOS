e460083cee8017702c80bf24f30ec21c2022a37a
dayo2n <0217dayun@naver.com>
2023-02-26 02:56:35 +0900

    [FEAT][#76] 어드민 > 가입 보안코드 설정 GUI 적용

--- momoIOS/AdminSide/MoimManagement/InputSecurityCodeViewController.swift
+++ momoIOS/AdminSide/MoimManagement/InputSecurityCodeViewController.swift
@@ -18,22 +18,16 @@ class InputSecurityCodeViewController: UIViewController {
         return self.codeField.isEditing
     }
     
-    private lazy var titleLabel = CommonTitleLabel(labelText: "가입보안코드를\n등록해주세요.")
-    
-    private let codeField: UITextField = {
-        let field = UITextField()
-        field.font = UIFont.systemFont(ofSize: 40)
-        field.textAlignment = .center
-        return field
+    private lazy var titleLabel = CommonTitleLabel(labelText: "보안코드를\n등록해주세요")
+    private lazy var guideLabel: UILabel = {
+        let label = UILabel()
+        label.text = "회원들이 가입시 등록할 코드를 정해주세요."
+        label.font = .body16
+        label.textColor = .gray600
+        return label
     }()
-    
-    private let borderView: UIView = {
-        let border = UIView()
-        border.backgroundColor = .black
-        return border
-    }()
-    
-    private lazy var okButton = CommonActionButton(buttonTitle: "확인")
+    private let codeField = CommonTextField(placeholderText: "")
+    private lazy var nextButton = CommonActionButton(buttonTitle: "확인")
     
     // MARK: - Lifecycles
     
@@ -44,7 +38,12 @@ class InputSecurityCodeViewController: UIViewController {
         self.setupKeyboardNotifications()
         self.setupCustomNav()
         self.setupLayout()
+        self.setupAction()
         self.codeField.becomeFirstResponder()
+        
+        nextButton.backgroundColor = .gray600
+        nextButton.setTitleColor(UIColor(hex: 0xFFFFFF, alpha: 0.4), for: .normal)
+        nextButton.isEnabled = false
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -55,7 +54,7 @@ class InputSecurityCodeViewController: UIViewController {
     
     @objc func keyboardWillShow(_ notification: NSNotification) {
         if self.isEditingCode {
-            self.okButton.moveWithKeyboard(
+            self.nextButton.moveWithKeyboard(
                 willShow: true,
                 notification: notification,
                 safeAreaBottomInset: self.view.safeAreaInsets.bottom
@@ -64,14 +63,45 @@ class InputSecurityCodeViewController: UIViewController {
     }
 
     @objc func keyboardWillHide(_ notification: NSNotification) {
-        self.okButton.moveWithKeyboard(
+        self.nextButton.moveWithKeyboard(
             willShow: false,
             notification: notification,
             safeAreaBottomInset: self.view.safeAreaInsets.bottom
         )
     }
     
+    @objc func textFieldDidChanged(_ textField: UITextField) {
+        guard let text = textField.text else { return }
+        
+        if text.count == 0 {
+            nextButton.backgroundColor = .gray600
+            nextButton.setTitleColor(UIColor(hex: 0xFFFFFF, alpha: 0.4), for: .normal)
+            nextButton.isEnabled = false
+        } else {
+            nextButton.backgroundColor = .main
+            nextButton.setTitleColor(.white, for: .normal)
+            nextButton.isEnabled = true
+        }
+    }
+    
+    @objc func textFieldDidBeginEditing(_ textField: UITextField) {
+        codeField.backgroundColor = UIColor.textbox2
+        codeField.layer.borderWidth = 1
+        codeField.layer.borderColor = UIColor.stroke.cgColor
+    }
+    
+    @objc func textFieldDidEndEditing(_ textField: UITextField) {
+        codeField.backgroundColor = .textbox1
+        codeField.layer.borderWidth = 0
+    }
+    
     // MARK: - Helpers
+    
+    private func setupAction() {
+        self.codeField.addTarget(self, action: #selector(textFieldDidChanged), for: .editingChanged)
+        self.codeField.addTarget(self, action: #selector(textFieldDidBeginEditing), for: .editingDidBegin)
+        self.codeField.addTarget(self, action: #selector(textFieldDidEndEditing), for: .editingDidEnd)
+    }
     
     private func setupKeyboardNotifications() {
         NotificationCenter.default.addObserver(
@@ -95,30 +125,35 @@ class InputSecurityCodeViewController: UIViewController {
         navBar?.backIndicatorImage = UIImage(systemName: "arrow.left")
         navBar?.backIndicatorTransitionMaskImage = UIImage(systemName: "arrow.left")
         navBar?.topItem?.title = ""
+        
+        let titleView = UILabel()
+        titleView.text = "활동시작"
+        titleView.textColor = .gray800
+        titleView.font = .body16
+        navigationItem.titleView = titleView
     }
     
     private func setupLayout() {
-        view.addSubviews(titleLabel, codeField, borderView, okButton)
+        view.addSubviews(titleLabel, guideLabel, codeField, nextButton)
         
         titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(68)
-            make.left.equalToSuperview().offset(24)
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(27)
+            make.leading.equalToSuperview().inset(24)
+        }
+        
+        guideLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(115)
+            make.leading.equalToSuperview().inset(24)
         }
         
         codeField.snp.makeConstraints { make in
-            make.left.right.equalToSuperview().inset(24)
-            make.top.equalTo(titleLabel.snp.bottom).offset(90)
+            make.horizontalEdges.equalToSuperview().inset(24)
+            make.top.equalTo(guideLabel.snp.bottom).offset(31)
         }
         
-        borderView.snp.makeConstraints { make in
-            make.top.equalTo(codeField.snp.bottom).offset(8)
-            make.left.right.equalToSuperview().inset(20)
-            make.height.equalTo(0.75)
-        }
-        
-        okButton.snp.makeConstraints { make in
+        nextButton.snp.makeConstraints { make in
             make.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(20)
-            make.left.right.equalToSuperview().inset(24)
+            make.horizontalEdges.equalToSuperview().inset(24)
         }
     }
 }
