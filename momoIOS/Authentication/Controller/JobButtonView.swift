f0158c61096bd809e3dc7d339f439649a5d1f1ea
dayo2n <0217dayun@naver.com>
2023-02-25 16:49:56 +0900

    [FEAT][#58] 가입 보안코드 입력 GUI

---
+++ momoIOS/Authentication/Controller/JobButtonView.swift
@@ -0,0 +1,72 @@
+//
+//  JobButtonView.swift
+//  momoIOS
+//
+//  Created by 문다 on 2023/02/25.
+//
+
+import UIKit
+
+class JobButtonView: UIView {
+    
+    // MARK: - Properties
+    var jobTag: Int?
+    
+    lazy var jobLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.textColor = .gray500
+        label.font = .systemFont(ofSize: 16, weight: .medium)
+        return label
+    }()
+    
+    lazy var jobImageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
+        return imageView
+    }()
+    
+    // MARK: - Lifecycles
+    
+    init(frame: CGRect, jobName: String, jobTag: Int) {
+        super.init(frame: frame)
+        self.jobTag = jobTag
+        jobLabel.text = jobName
+        jobLabel.textColor = jobTag == 1 ? .main : .gray500
+        jobImageView.image = UIImage(named: jobName.lowercased())
+        
+        self.backgroundColor = jobTag == 1 ? UIColor.rgba(237, 234, 255, 1) : .white
+        self.setupLayout()
+        
+        self.layer.masksToBounds = true
+        self.layer.borderColor = jobTag == 1 ? UIColor.main.cgColor : UIColor.pastbox.cgColor
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 8
+        self.tag = tag
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        self.backgroundColor = .white
+        self.setupLayout()
+    }
+    
+    // MARK: - Helpers
+    
+    private func setupLayout() {
+        self.addSubviews(jobLabel, jobImageView)
+        
+        jobImageView.snp.makeConstraints { make in
+            make.width.equalTo(80)
+            make.height.equalTo(80)
+            make.top.equalTo(self.snp.top).offset(33)
+            make.centerX.equalTo(self)
+        }
+
+        jobLabel.snp.makeConstraints { make in
+            make.top.equalTo(jobImageView.snp.bottom).offset(20)
+            make.centerX.equalTo(self)
+            make.bottom.equalTo(self.snp.bottom).inset(28)
+        }
+    }
+}
